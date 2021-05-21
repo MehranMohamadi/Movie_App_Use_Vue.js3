35e139c4e0cee6779951481208089d8b10e20cbf
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-05-21 20:41:59 +0430

    style: App.vue style

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,34 @@
 <template>
+<header>
+  <router-link to="/">
+    <h1>
+      <span>
+        Vue
+      </span>
+      Movies
+    </h1>
+  </router-link>
+</header>
+  <main>
+    <router-view/>
+  </main>
 
 
-  <router-view/>
 </template>
 
 <style lang="scss">
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family:'Fira Sans',sans-serif ;
 
+  &::selection{
+    background: transparentize(#42B883,0.5);
+  }
+
+  body{
+    background-color: #35595e;
+  }
+}
 </style>
