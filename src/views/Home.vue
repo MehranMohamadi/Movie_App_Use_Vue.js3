0f8aaab3d35857a1a0af7adf09270231f7a08744
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-05-21 20:56:55 +0430

    build:html of Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,20 @@
 <template>
-  <div class="home">
- Home
-  </div>
+    <div class="home">
+        <div class="Home">
+            <div class="feature_card">
+                <router-link to="/movie/tt0409591"></router-link>
+                <img src="" alt="Naruto Poster" class="feature-img">
+                <div class="detail">
+                    <h3>Spider-Man</h3>
+                    Spider-Man is a superhero created by writer-editor Stan Lee and writer-artist Steve Ditko. He first appeared in the anthology comic book Amazing Fantasy #15 in the Silver Age of Comic Books.
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 
 
-export default {
-
-
-}
+    export default {}
 </script>
