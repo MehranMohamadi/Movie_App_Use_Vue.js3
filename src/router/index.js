d9b0c767f24b84f2ab2970ae870d3d8d280258ef
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-05-22 23:19:37 +0430

    add:add movie detail and create a route

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
+import Home from '../views/MovieDetail.vue'
 
 const routes = [
     {path: '/', name: 'Home', component: Home},
-
+    {path:'/movie/:id',name:"Movie detail",component: MovieDetail}
 ]
 
 const router = createRouter({
