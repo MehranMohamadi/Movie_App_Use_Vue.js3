4d88261e39a3c4a32e01f2db0192c56e6210bfae
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-05-30 17:29:00 +0430

    script:search_box

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,8 +13,8 @@
                     </div>
                 </router-link>
             </div>
-            <form @submit.prevent="" class="search-box">
-                <input type="text" placeholder="What are you looking for?">
+            <form @submit.prevent="searchmovie()" class="search-box">
+                <input type="text" placeholder="What are you looking for?" v-model="search"/>
                 <input type="submit" value="Search">
             </form>
             <div class="Movie-list">Movies</div>
@@ -23,9 +23,28 @@
 </template>
 
 <script>
+    import {ref} from 'vue';
+    import env from "../env";
+
+    export default {
+        setup() {
+            const search = ref("");
+            const movies = ref([]);
+
+            const searchmovie = () => {
+                if (search.value !== "") {
+                    fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+                    .then(response=>response.json())
+                    .then(data=>{
+                        console.log(data);
+                    })
+                }
+            }
 
 
-    export default {}
+            return {search, movies, searchmovie}
+        }
+    }
 </script>
 
 
