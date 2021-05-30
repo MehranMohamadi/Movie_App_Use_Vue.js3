ce5aff1ae16b9bd399f1c730cd0b221fcf95fa8f
Mehran Mohammadi <mohammadi000098@gmail.com>
2021-05-30 21:52:52 +0430

    script:MovieDetail.vue

--- src/views/MovieDetail.vue
+++ src/views/MovieDetail.vue
@@ -1,12 +1,39 @@
-
 <template>
-<div class="Movie_Detail">Detail {{$route.params.id}}</div>
+    <div class="Movie_Detail">
+        <h2>{{movie.Title}}</h2>
+        <p>{{movie.Year}}</p>
+        <img :src="movie.Poster" alt="Movieposter" class="feature-img"/>
+        <p>{{movie.Plot}}</p>
+    </div>
 </template>
 
 <script>
+    import {ref, onBeforeMount} from 'vue';
+    import {useRoute} from 'vue-router';
+    import env from "../env";
+
+    export default {
+        setup() {
+            const movie = ref({});
+            const route = useRoute();
 
 
-    export default {}
+            onBeforeMount(() => {
+                fetch(`http://www/ombdapi.com/?apikey=${env.apikey}$i=${route.params.id}&plot
+                =full`)
+                    .then(response => response.json())
+                    .then(data => {
+                        movie.value = data;
+                    });
+
+
+            });
+
+            return {movie}
+
+
+        }
+    }
 </script>
 
 
